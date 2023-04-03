e3a8e50b64580681b3fd784442fcf3c5fcbf2a2c
Packetloosov <ignatievmarc@gmail.com>
2023-04-03 18:40:55 +0600

    qwe

---
+++ qwe.py
@@ -0,0 +1,14 @@
+a = int(input("Введите а"))
+h = int(input("Введите h"))
+def f(a, h):
+	S = a*h*0,5
+	print(S)
+f(a, h)
+
+q = int(input("Введите 1 сторону"))
+w = int(input("Введите 2 сторону"))
+e = int(input("Введите 3 сторону"))
+def f(q, w, e):
+	P = q + w + e
+	print(P)
+f(q, w, e)
